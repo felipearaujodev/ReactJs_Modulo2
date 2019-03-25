b70e17b6e09a4c6fe661e5bbf2f4f1a700a9df08
Felipe Araujo <comunicar.felipearaujo@gmail.com>
2019-03-25 08:00:41 -0300

    ReactJS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
+const Title = styled.h1`
+  color: #f00;
+  font-size: 32px;
+`;
+
 const App = () => {
   <div className="App">
-    <h1>Hello Word</h1>
+    <Title>Hello Word</Title>
   </div>;
 };
 
